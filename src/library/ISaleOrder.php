649ef0ffcb31cfea1e2e72f01fc871920b0ed68e
gjianbo <gjianbo@qq.com>
2018-08-17 14:57:31 +0800

    修改了批量创建销售订单时的bug

--- src/library/ISaleOrder.php
+++ src/library/ISaleOrder.php
@@ -21,7 +21,6 @@ class ISaleOrder extends IBaseSdk
         'create'=>'dto',
         'update'=>'dto',
         'delete'=>'dto',
-        'createBatch' => 'dto'
     ];
     protected $_opAction = [
         // 查询
@@ -39,19 +38,16 @@ class ISaleOrder extends IBaseSdk
     /**
      * 添加数据接口,以完成默认操作,必须在子类调用,外部不能直接访问
      * @param array $data 要添加的数据
-     * @param string $perfix 操作的前缀如: param ,dto
-     * @param bool $batch 批量添加 true 批量  false 单条
      * @return mixed
      */
-    public function createBatch($data = [], $perfix = '', $batch = false)
+    public function createBatch($data = [])
     {
         if (empty($data)) {
             return false;
         }
-        if (empty($perfix) && isset($this->param_prefix['createBatch'])) {
-            $perfix = $this->param_prefix['createBatch'];
-        }
-        $senddata = ($perfix . '=') . json_encode($data);
+
+        $senddata = '_args=' . json_encode($data);
+
         return $this->post_create($senddata);
     }
 
